10f8bd4ae74fc4e12ca9af2e4d7a46404067884e
CodeAlien <danylo.hreshchuk@gmail.com>
2019-10-15 16:36:07 +0300

    Add Vet Index page and Controller. Closes #14

---
+++ pet-clinic-web/src/main/java/org/codealien/petclinic/controller/VetController.java
@@ -0,0 +1,13 @@
+package org.codealien.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+	@RequestMapping({"/vets", "/vets/index", "/vets/index.hml"})
+	public String listVets() {
+		return "vets/index";
+	}
+}

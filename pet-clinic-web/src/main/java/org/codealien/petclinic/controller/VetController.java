4b134ee1ce7e8c1ad370cb2f5b3072013ce8ad01
CodeAlien <danylo.hreshchuk@gmail.com>
2019-10-16 13:17:47 +0300

    List all Vets on Vet Index Page. Closes #19

--- pet-clinic-web/src/main/java/org/codealien/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/org/codealien/petclinic/controller/VetController.java
@@ -1,13 +1,28 @@
 package org.codealien.petclinic.controller;
 
+import org.codealien.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+	
+
+	
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+
+
+
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.hml"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }

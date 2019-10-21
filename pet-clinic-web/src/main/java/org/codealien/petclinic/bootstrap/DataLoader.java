361573c2e5973308cc439414d0139e30e9a9e610
CodeAlien <danylo.hreshchuk@gmail.com>
2019-10-21 18:05:16 +0300

    Add PetTypes in Bootsrap. Closes #32

--- pet-clinic-web/src/main/java/org/codealien/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/codealien/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,11 @@
 package org.codealien.petclinic.bootstrap;
 
 import org.codealien.petclinic.model.Owner;
+import org.codealien.petclinic.model.PetType;
 import org.codealien.petclinic.model.Vet;
 import org.codealien.petclinic.service.OwnerService;
+import org.codealien.petclinic.service.PetService;
+import org.codealien.petclinic.service.PetTypeService;
 import org.codealien.petclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +15,24 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType savedDogPetType = petTypeService.save(dog);
+
+		PetType cat = new PetType();
+		dog.setName("Cat");
+		PetType savedCatPetType = petTypeService.save(cat);
+
 		Owner owner1 = new Owner();
 		owner1.setName("Danylo");
 		owner1.setSurname("Hreshchuk");

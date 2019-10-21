1f4f2e986a26fb2fbe6512f083890759a02ee7b3
CodeAlien <danylo.hreshchuk@gmail.com>
2019-10-21 14:25:39 +0300

    Create Pet Type, Pet, and Visit Entities. Closes #28

--- pet-clinic-data/src/main/java/org/codealien/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/codealien/petclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package org.codealien.petclinic.model;
 
-public class Owner extends Person{
+import java.util.Set;
+
+public class Owner extends Person {
 
 	private static final long serialVersionUID = -4594784992630246905L;
+
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 
 }

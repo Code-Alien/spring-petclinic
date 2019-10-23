ec729382deeb444488b288b310a5e83ca87eb90b
CodeAlien <danylo.hreshchuk@gmail.com>
2019-10-23 22:03:36 +0300

    Create Visit Entity. Closes #41

--- pet-clinic-data/src/main/java/org/codealien/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/org/codealien/petclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package org.codealien.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -27,6 +31,9 @@ public class Pet extends BaseEntity {
 
 	@Column(name = "name")
 	private String name;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 
 	public PetType getPetType() {
 		return petType;
@@ -58,6 +65,14 @@ public class Pet extends BaseEntity {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 }
